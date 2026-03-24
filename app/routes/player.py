8665750015818d327b36c035cdc51f69a8ebc921
Alejandro Armas <alearm246@gmail.com>
2026-03-24 02:07:09 -0500

    Working on /connect endpoint and RAG system

--- app/routes/player.py
+++ app/routes/player.py
@@ -1,3 +1,41 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.player import Player
+from app.models.user import User
+from app.services.clash import get_player_info, get_battle_log
+from app import db
 
 player_bp = Blueprint('player', __name__)
+
+@player_bp.route('/connect', methods=['POST'])
+@jwt_required()
+def connect_player_account():
+    data = request.get_json()
+    player_tag = data.get('player_tag')
+
+    player = get_player_info(player_tag)
+
+    if not player:
+        return jsonify({'message': 'player with player tag does not exist'}), 400
+
+    user_id = get_jwt_identity()
+
+    existing_player = Player.query.filter_by(player_tag=player_tag).first()
+
+    if existing_player and existing_player.user_id == user_id:
+        return jsonify({'message': 'this player tag is already associated with this account'}), 400
+    
+    try:
+        new_player = Player(user_id=user_id, player_tag=player_tag, player_name=player.get('name'))
+
+        db.session.add(new_player)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'message': 'Server error'}), 500
+
+    #get battle logs and store embeddings
+
+    return jsonify({'player_tag': player_tag}), 201 
+
+
---
+++ app/services/rag.py
@@ -0,0 +1,33 @@
+from app.services.clash import serialize_match, get_battle_log
+from app.services.embeddings import embed_text
+from app.models.player import Player
+from app.models.match import Match
+from datetime import datetime
+from app import db
+
+def store_match_embeddings(matches, player_id):
+    try:
+        for match in matches:
+            match_text = serialize_match(match)
+            embedding = embed_text(match_text)
+
+            player_battle_data = match['team'][0]
+            opponent_battle_data = match['opponent'][0]
+
+            player_crowns = player_battle_data['crowns']
+            opponent_crowns = opponent_battle_data['crowns']
+            result = 'won' if player_crowns > opponent_crowns else 'lost'
+            match_date = datetime.strptime(match.get('battleTime'), '%Y%m%dT%H%M%S.%fZ')
+
+            match_row = Match(player_id=player_id, raw_data=match, 
+                    natural_language_text=match_text, embedding=embedding,
+                    result=result, match_date=match_date)
+            db.session.add(match_row)
+            
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(f'Error: {e}')
+        return None
+
+    
